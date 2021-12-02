a3eca6775a78c384431ab13154e71608dc3db5a5
edelans <edelans@gmail.com>
2021-12-02 16:19:58 +0100

    part 1 ok

---
+++ aoc2021/day02.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""This script solves puzzles of https://adventofcode.com/"""
+
+import os
+import sys
+from aoc_utilities import Input, test_input
+
+# 2 digit day fetched from filename
+DAY = os.path.basename(__file__)[3:5]
+
+
+def solve1(data):
+    instructions = [(x.split(" ")) for x in data.splitlines()]
+    hpos, vpos = (0,0)
+    for inst in instructions:
+        if inst[0] == "forward":
+            hpos += int(inst[1])
+        elif inst[0] == "up":
+            vpos -= int(inst[1])
+        elif inst[0] == "down":
+            vpos += int(inst[1])
+    return hpos * vpos
+
+
+
+def solve2(data):
+    """Solves part2."""
+    pass
+
+
+"""
+Use script args to execute the right function.
+"""
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '1':
+        res = solve1((Input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '1t':
+        res = solve1((test_input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '2':
+        res = solve2((Input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '2t':
+        res = solve2((test_input(DAY).read()))
+        print(res)
