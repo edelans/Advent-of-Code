65f8d0753bb33394d5d33da706f806f9fdb5ec12
edelans <edelans@gmail.com>
2021-12-03 14:54:21 +0100

    part 2 ok

--- aoc2021/day03.py
+++ aoc2021/day03.py
@@ -31,10 +31,51 @@ def solve1(data):
 
     return bitsToInt(gammaBits) * bitsToInt(epsilonBits)
 
+
 def solve2(data):
     """Solves part2."""
-    pass
+    lines = [list(line) for line in data.splitlines()]
 
+    candidatesForO2gen = lines
+    candidatesForCO2scrub = lines
+
+    for index in range(len(lines[0])):
+        columns = [*zip(*candidatesForO2gen)]
+        howMany1s = columns[index].count("1")
+        howMany0s = len(columns[0]) - howMany1s
+
+        if howMany1s > howMany0s:
+            print(f"most common bit for column n°{index} is 1")
+            candidatesForO2gen = [x for x in candidatesForO2gen if x[index]=="1"]
+        elif howMany1s == howMany0s:
+            print(f"for column n°{index}, there are as many 1s as 0s, keeping 1s")
+            candidatesForO2gen = [x for x in candidatesForO2gen if x[index] == "1"]
+        else:
+            print(f"most common bit for column n°{index} is 0")
+            candidatesForO2gen = [x for x in candidatesForO2gen if x[index]=="0"]
+        if len(candidatesForO2gen) == 1:
+            print(f"most common bit for column n°{index} is 0")
+            break
+
+    for index in range(len(lines[0])):
+        columns = [*zip(*candidatesForCO2scrub)]
+        howMany1s = columns[index].count("1")
+        howMany0s = len(columns[0]) - howMany1s
+
+        if howMany0s > howMany1s:
+            print(f"least common bit for column n°{index} is 1")
+            candidatesForCO2scrub = [x for x in candidatesForCO2scrub if x[index]=="1"]
+        elif howMany1s == howMany0s:
+            print(f"for column n°{index}, there are as many 1s as 0s, keeping 0s")
+            candidatesForCO2scrub = [x for x in candidatesForCO2scrub if x[index] == "0"]
+        else:
+            print(f"least common bit for column n°{index} is 0")
+            candidatesForCO2scrub = [x for x in candidatesForCO2scrub if x[index]=="0"]
+        if len(candidatesForCO2scrub) == 1:
+            break
+
+
+    return bitsToInt(candidatesForO2gen[0]) * bitsToInt(candidatesForCO2scrub[0])
 
 """
 Use script args to execute the right function.
