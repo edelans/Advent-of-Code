6f011abb5a21e37d2d3cff038c3974596a0f0cd9
edelans <edelans@gmail.com>
2021-12-09 14:10:16 +0100

    part 2 in progress

--- aoc2021/day08.py
+++ aoc2021/day08.py
@@ -35,10 +35,53 @@ def solve1(data):
     return uniq_digit
 
 
+def decipher(signal_pattern, four_digit_output):
+    segments = {}
+    segments['one'] = set([x for x in signal_pattern if len(x) == 2])
+    segments['four'] = set([x for x in signal_pattern if len(x) == 4])
+    segments['seven'] = set([x for x in signal_pattern if len(x) == 3])
+    segments['eight'] = set([x for x in signal_pattern if len(x) == 7])
+
+
+    segments['two'] = set([x for x in signal_pattern if len(x) == 5])
+    segments['three'] = set([x for x in signal_pattern if len(x) == 5])
+    segments['five'] = set([x for x in signal_pattern if len(x) == 5])
+
+    segments['six'] = set([x for x in signal_pattern if len(x) == 6])
+    segments['zero'] = set([x for x in signal_pattern if len(x) == 6])
+    segments['nine'] = set([x for x in signal_pattern if len(x) == 6])
+
+    signal_line = {}
+    signal_line["top"] = set(segments['seven'][0]) - set(segments['one'][0])
+    signal_line["bottom"] = set(segments['six'][0]).intersection(set(segments['six'][1]), set(segments['six'][2])) - set(segments['seven'][0]) - set(segments['four'][0])
+
+    # trouver le 9 en faisant 4 + top + bottom
+    # trouver le 0 c'est celui à 6 char qui n'a qu'une linge de différence avec 9
+    # en déduire le 6 (le seul restant non identifié des 6 char)
+
+    # en déduire middle (9 - 0)
+    # en déduire bottom left (0 - 9)
+    # en déduire le 2
+    # en déduire le 3 : le seul qui a 1 de diff avec le 2
+    # en déduire le 5
+
+
+    signal_line["top_right"] = set(segments['one'][0])
+    signal_line["top_left"] =
+    signal_line["middle"] =
+    signal_line["bottom_right"] = set(segments['one'][0])
+    signal_line["bottom_left"] =
+
+
+
+    print(segments["nine"])
+
+
+
 def solve2(data):
     """Solves part2."""
-    pass
-
+    signal_patterns, four_digits_output_values = parser(data)
+    decipher(signal_patterns[0], four_digits_output_values[0])
 
 """
 Use script args to execute the right function.
