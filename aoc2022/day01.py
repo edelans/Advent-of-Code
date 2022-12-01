a0fd4888bf23dd0d28fd6a44e2b5a3b5549c2ea2
edelans <edelans@gmail.com>
2022-12-02 00:22:45 +0100

    part 1 ok

---
+++ aoc2022/day01.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""This script solves puzzles of https://adventofcode.com/"""
+
+import os
+import sys
+from aoc_utilities import Input, test_input
+
+# 2 digit day fetched from filename
+DAY = os.path.basename(__file__)[3:5]
+
+
+def solve1(data):
+    """Solves part 1."""
+    max_calories = 0
+    elves = data.split("\n\n")
+    for elf in elves:
+        elf_calories = sum([int(meal) for meal in elf.splitlines()])
+        if elf_calories > max_calories:
+            max_calories = elf_calories
+    return max_calories
+
+
+
+def solve2(data):
+    """Solves part2."""
+    pass
+
+
+"""
+Use script args to execute the right function.
+"""
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '1':
+        res = solve1((Input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '1t':
+        res = solve1((test_input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '2':
+        res = solve2((Input(DAY).read()))
+        print(res)
+    if len(sys.argv) > 1 and sys.argv[1] == '2t':
+        res = solve2((test_input(DAY).read()))
+        print(res)
