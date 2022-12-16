511c2134bbe912451cb6646221ace98e68914f61
edelans <edelans@gmail.com>
2022-12-16 22:08:58 +0100

    some marginal improvement

--- aoc2022/day16.py
+++ aoc2022/day16.py
@@ -57,6 +57,10 @@ def max_path(state):
     closed_valves = [n for n in G.nodes() if G.nodes[n]["closed"]]
     remaining_rates = sorted([G.nodes[n]["rate"] for n in closed_valves], reverse=True)
 
+    if remaining_rates[0] == 0:
+        # all valves with positive flow rates are already open, nothing to gain anymore
+        return state
+
     potential = 0
     for i, time in enumerate(range(remaining_time - 1, 0, -2)):
         potential += remaining_rates[i] * time
@@ -128,9 +132,91 @@ def solve1(data):
     return max_path(initial_state)
 
 
+def max_path2(state):
+    global CURRENT_MAX_EXPECTATION
+    G = state["G"]
+    # logger.info(f"current path length is {len(state['path'])}")
+
+    # pruning
+    remaining_time = TIME_LIMIT - len(state["path"])
+
+    closed_valves = [n for n in G.nodes() if G.nodes[n]["closed"]]
+    remaining_rates = sorted([G.nodes[n]["rate"] for n in closed_valves], reverse=True)
+
+    if remaining_rates[0] == 0:
+        # all valves with positive flow rates are already open, nothing to gain anymore
+        return state
+
+    potential = 0
+    for i, time in enumerate(range(remaining_time - 1, 0, -2)):
+        potential += remaining_rates[i] * time
+        if i + 2 > len(remaining_rates):
+            break
+
+    logger.info(
+        f"potential is {potential}, expected final pressure is {state['pressure_released']} current max is {CURRENT_MAX_EXPECTATION} , remaining time {remaining_time}"
+    )
+    if state["pressure_released"] + potential < CURRENT_MAX_EXPECTATION:
+        # drop this branch
+        logger.info(
+            f"droping path {state['path']}, cause potential {potential} is too low to catch up {CURRENT_MAX_EXPECTATION} with remaining time {remaining_time} while at pressure {state['pressure_released']}"
+        )
+        return {"path": [], "current_node": "AA", "pressure_released": 0, "G": G}
+
+    else:
+        if len(state["path"]) < TIME_LIMIT:
+            actions = []
+            if (
+                G.nodes[state["current_node"]]["closed"]
+                and G.nodes[state["current_node"]]["rate"] >= 1
+            ):
+                # open the valve
+                newG = G.copy()
+                newG.nodes[state["current_node"]]["closed"] = False
+                final_pressure_expectation = (
+                    state["pressure_released"]
+                    + (30 - len(state["path"]) - 1)
+                    * G.nodes[state["current_node"]]["rate"]
+                )
+                actions.append(
+                    max_path(
+                        {
+                            "path": state["path"]
+                            + [f"open valve {state['current_node']}"],
+                            "current_node": state["current_node"],
+                            "pressure_released": final_pressure_expectation,
+                            "G": newG,
+                        }
+                    )
+                )
+
+                if final_pressure_expectation > CURRENT_MAX_EXPECTATION:
+                    CURRENT_MAX_EXPECTATION = final_pressure_expectation
+            for n in G.neighbors(state["current_node"]):
+                # move to neighbor n
+                actions.append(
+                    max_path(
+                        {
+                            "path": state["path"] + [f"go to node {n}"],
+                            "current_node": n,
+                            "pressure_released": state["pressure_released"],
+                            "G": G,
+                        }
+                    )
+                )
+
+            return max(actions, key=lambda a: a["pressure_released"])
+
+        else:
+            return state
+
+
 def solve2(data):
     """Solves part2."""
     pass
+    G = parser(data)
+    initial_state = {"path": [], "current_node": "AA", "pressure_released": 0, "G": G}
+    return max_path2(initial_state)
 
 
 """
