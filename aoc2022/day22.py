43d5b01fd5464f4eb56bf1b1b21d311d6da84af7
edelans <edelans@gmail.com>
2022-12-23 02:14:12 +0100

    part 2 ok on test input... did not notice real input was different shape...

--- aoc2022/day22.py
+++ aoc2022/day22.py
@@ -131,9 +131,137 @@ def solve1(data):
     # 34 430 too low
 
 
+def move2(board, pos, facing, n):
+    x, y = pos
+    board[(x, y)] = DIR_FACING[facing]
+    square_size = len([x for x, y in board.keys() if y == 1])
+
+    for i in range(n):
+        if facing == 0:
+            dx, dy = 1, 0
+        elif facing == 1:
+            dx, dy = 0, 1
+        elif facing == 2:
+            dx, dy = -1, 0
+        elif facing == 3:
+            dx, dy = 0, -1
+
+        # compute potential new coordinates
+        if (x + dx, y + dy) in board.keys():
+            new_x, new_y = x + dx, y + dy
+            new_facing = facing
+        else:
+            # wrap around to the other side of the board
+
+            # moving RIGHT
+            if facing == 0 and y // square_size == 0 and x == 3 * square_size:
+                # from 1 to 6
+                new_x = 4 * square_size - (x - 2 * square_size)
+                new_y = 3 * square_size - y + 1
+                new_facing = 2
+            if facing == 0 and y // square_size == 1 and x == 3 * square_size:
+                # from 4 to 6
+                print("going from 4 to 6")
+                new_x = 5 * square_size - y + 1
+                new_y = 2 * square_size + 1
+                new_facing = 1
+            if facing == 0 and y // square_size == 2 and x == 4 * square_size:
+                # from 6 to 1
+                new_x = 3 * square_size
+                new_y = square_size - (y - 2 * square_size) + 1
+                new_facing = 2
+
+            # moving DOWN
+            elif facing == 1 and x // square_size == 0 and y == 2 * square_size:
+                # from 2 to 5
+                new_x = 2 * square_size + x
+                new_y = 3 * square_size
+                new_facing = 3
+            elif facing == 1 and x // square_size == 1 and y == 2 * square_size:
+                # from 3 to 5
+                new_x = 2 * square_size + 1
+                new_y = 3 * square_size - (x - square_size)
+                new_facing = 0
+            elif facing == 1 and x // square_size == 2 and y == 3 * square_size:
+                # from 5 to 2
+                new_x = 3 * square_size - x + 1
+                new_y = 2 * square_size
+                new_facing = 3
+            elif facing == 1 and x // square_size == 3 and y == 3 * square_size:
+                # from 6 to 2
+                new_x = 1
+                new_y = 5 * square_size - x
+                new_facing = 0
+
+            # moving LEFT
+            elif facing == 2 and y // square_size == 0 and x == 2 * square_size + 1:
+                # from 1 to 3
+                new_x = y + square_size
+                new_y = square_size + 1
+                new_facing = 1
+            elif facing == 2 and y // square_size == 1 and x == 1:
+                # from 2 to 6
+                new_x = 5 * square_size - y
+                new_y = 3 * square_size
+                new_facing = 0
+            elif facing == 2 and y // square_size == 2 and x == 2 * square_size + 1:
+                # from 5 to 3
+                new_x = 4 * square_size - y
+                new_y = 2 * square_size
+                new_facing = 3
+
+            # moving UP
+            elif facing == 3 and x // square_size == 0 and y == square_size + 1:
+                # from 2 to 1
+                new_x = 2 * square_size + 1
+                new_y = 3 * square_size - x
+                new_facing = 1
+            elif facing == 3 and x // square_size == 1 and y == square_size + 1:
+                # from 3 to 1
+                new_x = 2 * square_size + 1
+                new_y = x - square_size
+                new_facing = 0
+            elif facing == 3 and x // square_size == 2 and y == 1:
+                # from 1 to 2
+                new_x = 3 * square_size - y
+                new_y = square_size + 1
+                new_facing = 1
+            elif facing == 3 and x // square_size == 3 and y == 2 * square_size + 1:
+                # from 6 to 4
+                new_x = 3 * square_size
+                new_y = 5 * square_size - x
+                new_facing = 2
+
+        if board[(new_x, new_y)] == "#":
+            # if we are in a wall, stop moving
+            break
+        else:
+            # keep moving!
+            x, y = new_x, new_y
+            facing = new_facing
+            board[(x, y)] = DIR_FACING[facing]
+
+    return board, (x, y), facing
+
+
 def solve2(data):
     """Solves part2."""
-    pass
+    board, path = parser(data)
+    pos = (min([x for (x, y) in board.keys() if y == 1]), 1)
+    facing = 0
+    for ins in path:
+        if isinstance(ins, int):
+            board, pos, facing = move2(board, pos, facing, ins)
+        else:
+            facing = turn(facing, ins)
+        print(f"after move {ins}, position is {pos} with facing {facing}")
+        print(dump(board))
+    return 1000 * pos[1] + 4 * pos[0] + facing
+    # I did like any noob and wrote up all the edge connections for the test case,
+    # it works....
+    # and only then I found the input was in a different shape (WTF!)
+
+    # elegant generic strategy here : https://www.reddit.com/r/adventofcode/comments/zsct8w/comment/j18dzaa/?utm_source=share&utm_medium=web2x&context=3
 
 
 """
