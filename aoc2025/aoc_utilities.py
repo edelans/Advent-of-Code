f135e56c52177ec3f47fb4389a9c781eb3c11c9b
edelans <edelans@gmail.com>
2025-12-01 21:59:27 +0100

    init for 2025, YAY!

---
+++ aoc2025/aoc_utilities.py
@@ -0,0 +1,100 @@
+import re
+from itertools import product
+import operator, time
+
+OPS = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+    "%": operator.mod,
+    "^": operator.xor,
+}
+
+
+def parse_words(text):
+    "All the words in text"
+    return text.split(",")
+
+
+def Input(day):
+    "Open this day's input file."
+    filename = "./input_files/input{}.txt".format(day)
+    return open(filename)
+
+
+def test_input(day):
+    "Open this day's test input file"
+    filename = "./input_files/input{}.test.txt".format(day)
+    return open(filename)
+
+
+def neighbors_all(point):
+    """returns a set of all the neighboring positions of the input point
+    whatever the number of dimentions
+
+    Examples :
+    >>> import aoc_utilities
+    >>> aoc_utilities.neighbors_all((0,0))
+    {(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, 0), (1, -1), (1, 1)}
+    >>> aoc_utilities.neighbors_all((0,0,0))
+    {(0, 1, 1), (1, -1, -1), (1, 0, 0), (-1, 0, -1), (0, 0, 1), (0, -1, 1), (1, 0, 1), (-1, -1, -1), (-1, 1, -1), (0, -1, 0), (1, 1, 1), (-1, 1, 0), (1, 1, 0), (1, 1, -1), (-1, 1, 1), (-1, -1, 1), (1, -1, 0), (0, -1, -1), (-1, -1, 0), (0, 0, -1), (-1, 0, 1), (1, 0, -1), (1, -1, 1), (-1, 0, 0), (0, 1, 0), (0, 1, -1)}
+    >>> len(aoc_utilities.neighbors_all((0,0)))
+    8
+    >>> len(aoc_utilities.neighbors_all((0,0,0)))
+    26
+    >>> len(aoc_utilities.neighbors_all((0,0,0,0)))
+    80"""
+
+    ranges = ((c - 1, c, c + 1) for c in point)
+    return set(product(*ranges)) - set([point])
+
+
+def neighbors_4(pos):
+    """
+    returns positions of 4 neighbors : up, down, left, right
+    Be careful as these positions can be outside your map
+    """
+    return [
+        (pos[0] - 1, pos[1]),
+        (pos[0] + 1, pos[1]),
+        (pos[0], pos[1] - 1),
+        (pos[0], pos[1] + 1),
+    ]
+
+
+def mprint(maping, min_half_size=10):
+    """
+    Helper function to print a map
+    when the map is a dictionary, with keys as tuples of coordinates (1,2)
+    no need to have all the coordinates in the keys
+    """
+    xmax = max(max([int(i) for (i, j) in maping.keys()]), min_half_size)
+    xmin = min(min([int(i) for (i, j) in maping.keys()]), -min_half_size)
+    ymax = max(max([int(j) for (i, j) in maping.keys()]), min_half_size)
+    ymin = min(min([int(j) for (i, j) in maping.keys()]), -min_half_size)
+    for y in range(ymax, ymin - 1, -1):
+        print("".join([str(maping.get((x, y), ".")) for x in range(xmin, xmax + 1)]))
+    return
+
+
+def sign(x):
+    """
+    note : sometimes it can be more useful to use math.copysign()
+    see https://stackoverflow.com/a/1986718/1570104
+    """
+    return bool(x > 0) - bool(x < 0)
+
+
+def timer_func(func):
+    # This function shows the execution time of
+    # the function object passed
+    # (to be used as a wraper)
+    def wrap_func(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f"Function {func.__name__!r} executed in {(t2 - t1):.4f}s")
+        return result
+
+    return wrap_func
---
+++ aoc2025/readme.md
@@ -0,0 +1,28 @@
+# Why
+
+Just having fun sovling [advent of code](https://adventofcode.com/) puzzles one more year =)
+
+# Goal of this year
+
+Experiment with AI IDEs.
+
+# Repos to watch for instpiration
+
+- https://github.com/mebeim/aoc/tree/master/2021
+
+# Learnings
+
+# Usage
+
+- don't forget to install a virtualenv (will be based on pyenv current python version)
+
+```python
+  # Create a virtual environment
+  python3 -m venv venv
+
+  # Activate it (macOS/Linux)
+  source venv/bin/activate
+
+  # Deactivate
+  deactivate
+```
