a37900fe26780a945f395fbe3af74f03b4f8afb5
edelans <edelans@gmail.com>
2025-12-01 23:21:14 +0100

    add ruff linter and formatter

--- aoc2025/day01.py
+++ aoc2025/day01.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """This script solves puzzles of https://adventofcode.com/"""
 
+import logging
 import os
 import sys
-import logging
-from aoc_utilities import Input, test_input
-
 from collections import deque
 
+from aoc_utilities import Input, test_input
 
 """
 Logger config
@@ -30,49 +29,47 @@ DAY = os.path.basename(__file__)[3:5]
 
 def solve1(data):
     """Solves part 1."""
-    circle = deque(range(0,100))
-    circle.rotate(-50) # rotate to put 50 at the beginning of the circle
+    circle = deque(range(0, 100))
+    circle.rotate(-50)  # rotate to put 50 at the beginning of the circle
     logger.info(f"The dial points at {circle[0]}.")
-    counter=0
+    counter = 0
     for line in data.splitlines():
-        (d,nb) = (line[0], int(line[1:]))
+        (d, nb) = (line[0], int(line[1:]))
         logger.info(f"The dial points at {circle[0]}. Will rotate to the {d} by {nb}")
         if d == "R":
             circle.rotate(-nb)
         if d == "L":
             circle.rotate(+nb)
-         
-        if circle[0]==0:
+
+        if circle[0] == 0:
             counter += 1
-    logger.info(f"The dial points at {circle[0]}. We have seen {counter} times the number 0.")
-    return counter 
-
-
-
-
-
-        
+    logger.info(
+        f"The dial points at {circle[0]}. We have seen {counter} times the number 0."
+    )
+    return counter
 
 
 def solve2(data):
     """Solves part2."""
-    circle = deque(range(0,100))
-    circle.rotate(-50) # rotate to put 50 at the beginning of the circle
+    circle = deque(range(0, 100))
+    circle.rotate(-50)  # rotate to put 50 at the beginning of the circle
     logger.info(f"The dial points at {circle[0]}.")
-    counter=0
+    counter = 0
     for line in data.splitlines():
-        (d,nb) = (line[0], int(line[1:]))
+        (d, nb) = (line[0], int(line[1:]))
         logger.info(f"The dial points at {circle[0]}. Will rotate to the {d} by {nb}")
         for i in range(nb):
             if d == "R":
                 circle.rotate(-1)
             if d == "L":
                 circle.rotate(+1)
-            if circle[0]==0:
+            if circle[0] == 0:
                 counter += 1
-                logger.info(f"We have seen the number 0!")
-    logger.info(f"The dial points at {circle[0]}. We have seen {counter} times the number 0.")
-    return counter 
+                logger.info("We have seen the number 0!")
+    logger.info(
+        f"The dial points at {circle[0]}. We have seen {counter} times the number 0."
+    )
+    return counter
 
 
 """
@@ -86,17 +83,17 @@ if __name__ == "__main__":
     """some logger levels : DEBUG, INFO, WARNING, CRITICAL"""
     if len(sys.argv) > 1 and sys.argv[1] == "1t":
         logger.setLevel(logging.INFO)
-        res = solve1((test_input(DAY).read()))
+        res = solve1(test_input(DAY).read())
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == "1":
         logger.setLevel(logging.WARNING)
-        res = solve1((Input(DAY).read()))
+        res = solve1(Input(DAY).read())
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == "2t":
         logger.setLevel(logging.INFO)
-        res = solve2((test_input(DAY).read()))
+        res = solve2(test_input(DAY).read())
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == "2":
         logger.setLevel(logging.WARNING)
-        res = solve2((Input(DAY).read()))
+        res = solve2(Input(DAY).read())
         print(res)
