0612ce8606fffab1b5b6c57cd4ea1cc965e5620a
edelans <edelans@gmail.com>
2025-12-03 14:57:24 +0100

    part 2 ok

--- aoc2025/day03.py
+++ aoc2025/day03.py
@@ -27,7 +27,7 @@ DAY = os.path.basename(__file__)[3:5]
 def largest_joltage(bank):
     batt1 = int(bank[0])
     batt2 = int(bank[1])
-    logger.debug(f"computing largest joltage for {bank}")
+    logger.info(f"\nComputing largest joltage for {bank}")
     for i in range(1, len(bank)):
         battery = int(bank[i])
         if battery > batt1 and i < len(bank) - 1:
@@ -58,6 +58,63 @@ def solve1(data):
     acc = 0
     for bank in data.splitlines():
         acc += largest_joltage(bank)
+    return acc
+
+
+def largest_joltage_generic(bank, target_joltage_size):
+    joltage = ""
+    available_bank = bank
+    logger.info(
+        f"\nComputing largest joltage for {bank} with target size {target_joltage_size}"
+    )
+    while len(joltage) < target_joltage_size:
+        available_bank, joltage, target_joltage_size = add_battery(
+            available_bank, joltage, target_joltage_size
+        )
+    return int(joltage)
+
+
+def add_battery(
+    available_bank: str, joltage: str, target_joltage_size: int
+) -> tuple[str, str, int]:
+    # let's get the max index and value in the initial slice of the bank that will allow us to reach the target_joltage_size
+    search_window = available_bank[
+        : len(available_bank) - (target_joltage_size - len(joltage)) + 1
+    ]
+    max_index, max_value = max(
+        enumerate(search_window),
+        key=lambda x: int(x[1]),
+    )
+    # update joltage and available_bank
+    newbank = available_bank[max_index + 1 :]
+    joltage = joltage + str(max_value)
+    logger.info(
+        f"adding battery {max_value}, new joltage is {joltage}, new bank: {newbank}, missing {target_joltage_size - len(joltage)} batteries, next search window will be {newbank[: len(newbank) - (target_joltage_size - len(joltage)) + 1]}"
+    )
+    return newbank, joltage, target_joltage_size
+
+
+def _test_largest_joltage12():
+    cases = (
+        ("987654321111111", 987654321111),
+        ("811111111111119", 811111111119),
+        ("234234234234278", 434234234278),
+        ("818181911112111", 888911112111),
+    )
+    for digits, expected in cases:
+        result = largest_joltage_generic(digits, 12)
+        assert result == expected, (
+            f"largest_joltage({digits}) -> {result}, expected {expected}"
+        )
+
+
+@timer_func
+def solve2(data):
+    """Solves part2."""
+    acc = 0
+    target_joltage_size = 12
+    for bank in data.splitlines():
+        acc += largest_joltage_generic(bank, target_joltage_size)
     return acc
 
 
