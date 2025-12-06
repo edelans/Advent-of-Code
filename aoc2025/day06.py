200c19a66e69605ca063cfb996b0610281604de5
edelans <edelans@gmail.com>
2025-12-06 13:29:42 +0100

    fugly part 2, but works

--- aoc2025/day06.py
+++ aoc2025/day06.py
@@ -25,9 +25,9 @@ logger.addHandler(handler)
 DAY = os.path.basename(__file__)[3:5]
 
 
-def parser(data: str) -> list[tuple[int, int]]:
+def parser(data: str) -> tuple[list[list[int]], list[str]]:
     lines = data.strip().splitlines()
-    ops = [op for op in lines[-1].split()]
+    ops = lines[-1].split()
     lines = lines[:-1]
     numbers = []
     for line in lines:
@@ -47,14 +47,85 @@ def solve1(data):
             acc += sum([n[x] for n in numbers])
         elif op == "*":
             acc += prod([n[x] for n in numbers])
-
     return acc
+
+
+def parser2(data: str) -> tuple[list[list[int]], list[str]]:
+    lines = data.splitlines()
+    ops = list(lines[-1])
+    lines = lines[:-1]
+    numbers = []
+    for line in lines:
+        numbers.append(list(line))
+    assert all(len(n) == len(numbers[0]) for n in numbers), (
+        "All items in numbers must have the same size"
+    )
+    return numbers, ops
+
+
+def transpose(matrix: list[list[int]]) -> list[list[int]]:
+    return [list(row) for row in zip(*matrix, strict=True)]
 
 
 @timer_func
 def solve2(data):
-    """Solves part2."""
-    pass
+    numbers, ops = parser2(data)
+    logger.info(f"numbers: {numbers}, \nops: {ops}")
+    acc = 0
+    submatrix = [[] for _ in range(len(numbers))]
+    logger.info(f"Submatrix: {submatrix}")
+    current_op = None
+    for i, op in enumerate(ops):
+        if op != " ":
+            # deal with the current submatrix
+            if current_op:
+                nbs = []
+                for row in transpose(submatrix):
+                    s = ""
+                    for c in row:
+                        s += c
+                    if len(s.strip()) > 0:
+                        nbs.append(int(s))
+
+                inc = 0
+                if current_op == "+":
+                    inc = sum(nbs)
+                elif current_op == "*":
+                    inc = prod(nbs)
+                logger.info(
+                    f"Computing problem applying {current_op} on {nbs} -> adding {inc} to the result"
+                )
+                acc += inc
+
+            # reinit submatrix and current_op
+            logger.info("\nStarting a new problem.")
+            submatrix = [[] for _ in range(len(numbers))]
+            current_op = op
+
+        for j in range(len(submatrix)):
+            logger.info(f"Adding {numbers[j][i]} to submatrix[{j}]")
+            submatrix[j].append(numbers[j][i])
+        logger.info(f"Submatrix: {submatrix}")
+
+    nbs = []
+    for row in transpose(submatrix):
+        s = ""
+        for c in row:
+            s += c
+        if len(s.strip()) > 0:
+            nbs.append(int(s))
+
+    inc = 0
+    if current_op == "+":
+        inc = sum(nbs)
+    elif current_op == "*":
+        inc = prod(nbs)
+    logger.info(
+        f"Computing problem applying {current_op} on {nbs} -> adding {inc} to the result"
+    )
+    acc += inc
+
+    return acc
 
 
 """
@@ -79,8 +150,8 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "2t":
         logger.setLevel(logging.INFO)
         res = solve2(test_input(DAY).read())
-        # expected = XXX  # TODO: replace with expected value
-        # assert res == expected, f"Expected {expected}, got {res}"
+        expected = 3263827  # TODO: replace with expected value
+        assert res == expected, f"Expected {expected}, got {res}"
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == "2":
         logger.setLevel(logging.WARNING)
