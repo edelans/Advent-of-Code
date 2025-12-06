3a2941b65a86e2f2b6095b4409e09d8654f9decc
edelans <edelans@gmail.com>
2025-12-06 13:48:50 +0100

    improve timer formatting ⏱️

--- aoc2025/aoc_utilities.py
+++ aoc2025/aoc_utilities.py
@@ -97,7 +97,7 @@ def timer_func(func):
         t1 = time.time()
         result = func(*args, **kwargs)
         t2 = time.time()
-        print(f"Function {func.__name__!r} executed in {(t2 - t1):.4f}s")
+        print(f"\n⏱️ Function {func.__name__!r} executed in {(t2 - t1):.4f}s")
         return result
 
     return wrap_func
