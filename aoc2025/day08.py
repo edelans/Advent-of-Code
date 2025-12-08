7ac840f3b5b6c41c0638ea8d45c7b488a4d1da9d
edelans <edelans@gmail.com>
2025-12-08 12:31:28 +0100

    part 2 ok

--- aoc2025/day08.py
+++ aoc2025/day08.py
@@ -106,7 +106,77 @@ def solve1(data, max_connections=1000):
 @timer_func
 def solve2(data):
     """Solves part2."""
-    pass
+    points = []
+    for line in data.splitlines():
+        points.append(tuple(int(i) for i in line.split(",")))
+
+    all_distances = []
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            all_distances.append(
+                tuple((points[i], points[j], distance(points[i], points[j])))
+            )
+    all_distances.sort(key=lambda x: x[2])
+
+    circuits = []
+
+    for i, d in enumerate(all_distances[0:]):
+        logger.info(f"\n#{i + 1} shortest connection is between {d[0]} and {d[1]}")
+        c0 = {d[0]}
+        c1 = {d[1]}
+        for c in circuits:
+            if d[0] in c:
+                c0 = c
+                logger.info(f"  - {d[0]} is already in circuit {c}")
+            if d[1] in c:
+                c1 = c
+                logger.info(f"  - {d[1]} is already in circuit {c}")
+        if c0 == c1:
+            logger.info(
+                f"  - {d[0]} and {d[1]} are already part of the same circuit -> no circuit change"
+            )
+        else:
+            if len(c0) == 1 and len(c1) > 1:
+                logger.info(f"  - {d[0]} is not part of any circuit, adding it to {c1}")
+                circuits.remove(c1)
+                c1.add(d[0])
+                circuits.append(c1)
+
+            elif len(c1) == 1 and len(c0) > 1:
+                logger.info(f"  - {d[1]} is not part of any circuit, adding it to {c0}")
+                circuits.remove(c0)
+                c0.add(d[1])
+                circuits.append(c0)
+
+            elif len(c0) == 1 and len(c1) == 1:
+                logger.info(
+                    f"  - {d[0]} and {d[1]} are not part of any circuit, creating a new circuit!"
+                )
+                circuits.append(c0.union(c1))
+            elif len(c0) > 1 and len(c1) > 1:
+                logger.info(
+                    f"  - {d[0]} and {d[1]} are not part of any circuit, creating a new circuit!"
+                )
+                circuits.remove(c0)
+                circuits.remove(c1)
+                circuits.append(c0.union(c1))
+        circuit_lengths = [len(c) for c in circuits]
+        if sum(circuit_lengths) == len(points):
+            logger.info("All points are in circuits!")
+            return d[0][0] * d[1][0]
+
+        logger.info(
+            f"  - current circuits lengths are: {sorted([len(c) for c in circuits], reverse=True)}"
+        )
+
+    logger.info("Final circuits are:")
+    for c in circuits:
+        logger.info(f"  - {c}")
+
+    logger.warning(f"Total number of circuits is {len(circuits)}")
+
+    logger.warning(f"3 largest circuit sizes are: {circuit_lengths[0:3]}")
+    return circuit_lengths[0] * circuit_lengths[1] * circuit_lengths[2]
 
 
 """
@@ -131,7 +201,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "2t":
         logger.setLevel(logging.INFO)
         res = solve2(test_input(DAY).read())
-        expected = XXX  # TODO: replace with expected value
+        expected = 25272  # TODO: replace with expected value
         assert res == expected, f"Expected {expected}, got {res}"
         print(res)
     if len(sys.argv) > 1 and sys.argv[1] == "2":
